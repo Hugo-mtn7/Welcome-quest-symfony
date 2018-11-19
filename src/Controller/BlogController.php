17de6a21e2cd02c0e66a01c73c77ac28f918866c
hugo-mtn7 <hugo.mtn7@gmail.com>
2018-11-19 15:42:05 +0100

    quest 8

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,6 +4,9 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Article;
+use App\Entity\Category;
 
 class BlogController extends AbstractController
 {
@@ -19,5 +22,51 @@ class BlogController extends AbstractController
             'title' => $title,
         ]);
 
+    }
+
+    /**  
+     * Show all row from article's entity
+     *  
+     * @Route("/", name="blog_index")  
+     * @return Response A response instance  
+     */
+    public function index() : Response  
+    {  
+        $articles = $this->getDoctrine()  
+            ->getRepository(Article::class)  
+            ->findAll();  
+
+        if (!$articles) {  
+            throw $this->createNotFoundException(  
+            'No article found in article\'s table.'
+            );  
+        }  
+
+        return $this->render(  
+                'blog/index.html.twig',  
+                ['articles' => $articles]  
+        );  
+    }
+
+    /**  
+     * Show all article from one category
+     *  
+     * @Route("/blog/category/{category}", name="blog_show_category")  
+     * @return Response A response instance  
+     */
+    public function showByCategory(string $category) : Response  
+    {  
+        $categoryToPrint = $this->getDoctrine()
+            ->getRepository(Category::class)
+            ->findOneByName($category);
+
+        $articles = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->findBy(['category' => $categoryToPrint->getId()]);
+
+        return $this->render(  
+                'blog/category.html.twig',  
+                ['articles' => $articles]  
+        );  
     }
 }
